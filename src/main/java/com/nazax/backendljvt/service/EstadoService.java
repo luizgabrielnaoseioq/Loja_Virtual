bb58658a95e707180cb3de42b205016d13b77ff3
Nazax <gabrielon6689@gmail.com>
2025-03-13 17:03:47 -0300

    Crud criado

--- src/main/java/com/nazax/backendljvt/service/EstadoService.java
+++ src/main/java/com/nazax/backendljvt/service/EstadoService.java
@@ -1,8 +1,12 @@
 package com.nazax.backendljvt.service;
 
+import com.nazax.backendljvt.entity.Estado;
 import com.nazax.backendljvt.repository.EstadoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,5 +14,22 @@ public class EstadoService {
 
     private final EstadoRepository estadoRepository;
 
+    public List<Estado> findAll(){
+        return estadoRepository.findAll();
+    }
 
+    public Estado inserir(Estado estado) {
+        estado.setDataCriacao(new Date());
+        return estadoRepository.saveAndFlush(estado);
+    }
+
+    public Estado atualizar(Estado estado) {
+        estado.setDataAtualizacao(new Date());
+        return estadoRepository.saveAndFlush(estado);
+    }
+
+    public void deletear(Long id) {
+        Estado estado = estadoRepository.findById(id).get();
+        estadoRepository.delete(estado);
+    }
 }
