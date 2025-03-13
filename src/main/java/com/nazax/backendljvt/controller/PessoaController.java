34ca2fd108cc6b93ace8f814376e69da1cc4728b
Nazax <gabrielon6689@gmail.com>
2025-03-13 19:46:57 -0300

    Metodos criados e funcionando

---
+++ src/main/java/com/nazax/backendljvt/controller/PessoaController.java
@@ -0,0 +1,37 @@
+package com.nazax.backendljvt.controller;
+
+import com.nazax.backendljvt.entity.Pessoa;
+import com.nazax.backendljvt.service.PessoaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pessoa")
+@RequiredArgsConstructor
+public class PessoaController {
+
+    private final PessoaService pessoaService;
+
+    @GetMapping("/")
+    public List<Pessoa> buscarPessoas() {
+        return pessoaService.buscarTodasPessoas();
+    }
+
+    @PostMapping("/")
+    public Pessoa criarPessoa(@RequestBody Pessoa pessoa) {
+        return pessoaService.criarPessoa(pessoa);
+    }
+
+    @PutMapping("/")
+    public Pessoa atualizarPessoa(@RequestBody Pessoa pessoa) {
+        return pessoaService.atualizarPessoa(pessoa);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarPessoa(@PathVariable("id") Long id) {
+        pessoaService.deletarPessoa(id);
+        return ResponseEntity.ok().build();
+    }
+}
