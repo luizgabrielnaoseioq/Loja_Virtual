4c6d41ef177ed5dc063202d7ab41f131958d56f0
Nazax <gabrielon6689@gmail.com>
2025-03-13 16:43:46 -0300

    Craido repository de estado.

---
+++ src/main/java/com/nazax/backendljvt/repository/EstadoRepository.java
@@ -0,0 +1,8 @@
+package com.nazax.backendljvt.repository;
+
+import com.nazax.backendljvt.entity.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstadoRepository extends JpaRepository<Estado, Long> {
+
+}
