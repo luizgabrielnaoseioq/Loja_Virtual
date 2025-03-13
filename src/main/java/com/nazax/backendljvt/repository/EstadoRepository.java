00ef4230c93a2616e51bdb658918cecdc54cc611
Nazax <gabrielon6689@gmail.com>
2025-03-13 17:52:29 -0300

    ignore

--- src/main/java/com/nazax/backendljvt/repository/EstadoRepository.java
+++ src/main/java/com/nazax/backendljvt/repository/EstadoRepository.java
@@ -2,7 +2,9 @@ package com.nazax.backendljvt.repository;
 
 import com.nazax.backendljvt.entity.Estado;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EstadoRepository extends JpaRepository<Estado, Long> {
 
 }
