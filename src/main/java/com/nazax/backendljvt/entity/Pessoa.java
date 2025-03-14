0dff8077e4943a3249b9897432f001d025e1f8d6
Nazax <gabrielon6689@gmail.com>
2025-03-14 14:06:58 -0300

    Adicionamos todas as anotações de validação.

--- src/main/java/com/nazax/backendljvt/entity/Pessoa.java
+++ src/main/java/com/nazax/backendljvt/entity/Pessoa.java
@@ -1,6 +1,8 @@
 package com.nazax.backendljvt.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.util.Date;
@@ -14,16 +16,23 @@ public class Pessoa {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "O nome é obrigatorio")
     private String nome;
 
+    @NotBlank(message = "O CPF é obrigatorio")
     private String cpf;
 
+    @NotBlank(message = "O  é obrigatorio")
+    @Email(message = "E-mail inválido")
     private String email;
 
+    @NotBlank(message = "A senha é obrigatorio")
     private String senha;
 
+    @NotBlank(message = "O endereço é obrigatorio")
     private String endereco;
 
+    @NotBlank(message = "O CEP é obrigatorio")
     private String cep;
 
     @Temporal(TemporalType.TIMESTAMP)
