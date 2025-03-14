b587948ae61ad5e6f3f1786e9490d446a4a0f852
Nazax <gabrielon6689@gmail.com>
2025-03-14 14:42:16 -0300

    Adcionei metodos de contrução

--- src/main/java/com/nazax/backendljvt/entity/Estado.java
+++ src/main/java/com/nazax/backendljvt/entity/Estado.java
@@ -1,9 +1,11 @@
 package com.nazax.backendljvt.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 import java.util.Date;
+import java.util.Objects;
 
 @Entity
 @Data
@@ -14,10 +16,32 @@ public class Estado {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "O Nome da cidade é obrigatorio!")
     private String nome;
     private String sigla;
     @Temporal(TemporalType.TIMESTAMP)
     private Date dataCriacao;
     @Temporal(TemporalType.TIMESTAMP)
     private Date dataAtualizacao;
+
+    public Estado(String nome, String sigla) {
+        this.nome = nome;
+        this.sigla = sigla;
+    }
+
+    public Estado() {
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Estado estado = (Estado) o;
+        return Objects.equals(id, estado.id) && Objects.equals(nome, estado.nome) && Objects.equals(sigla, estado.sigla) && Objects.equals(dataCriacao, estado.dataCriacao) && Objects.equals(dataAtualizacao, estado.dataAtualizacao);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, sigla, dataCriacao, dataAtualizacao);
+    }
 }
