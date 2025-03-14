d9bf8d7e1b1db51567a6e3dbd16aad2497e4efb6
Nazax <gabrielon6689@gmail.com>
2025-03-14 14:59:20 -0300

    Criado o repository de marca.

---
+++ src/main/java/com/nazax/backendljvt/repository/MarcaRepository.java
@@ -0,0 +1,7 @@
+package com.nazax.backendljvt.repository;
+
+import com.nazax.backendljvt.entity.Marca;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MarcaRepository extends JpaRepository<Marca, Long> {
+}
