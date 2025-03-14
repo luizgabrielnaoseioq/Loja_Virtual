98e9a9ee5f10b6b22440fa7a5aa018f052300664
Nazax <gabrielon6689@gmail.com>
2025-03-14 16:47:55 -0300

    Adição do @Repository

--- src/main/java/com/nazax/backendljvt/repository/MarcaRepository.java
+++ src/main/java/com/nazax/backendljvt/repository/MarcaRepository.java
@@ -2,6 +2,8 @@ package com.nazax.backendljvt.repository;
 
 import com.nazax.backendljvt.entity.Marca;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MarcaRepository extends JpaRepository<Marca, Long> {
 }
